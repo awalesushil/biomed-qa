9c9abf38202a20b92f007bb602cf26e361e19a04
Sushil Awale <ssushil.awale@gmail.com>
2022-07-05 22:43:38 +0200

    Revert "Components stub files"
    This reverts commit 92c84111a001e55085ca0c1e8d2d68741e1e480e.

--- app/qamodel/QAModel.py
+++
@@ -1,7 +0,0 @@
-class QAModel:
-
-    def __init__(self):
-        pass
-
-    def get_answers(self, passages):
-        raise NotImplementedError
--- app/queryformulator/QueryFormulator.py
+++
@@ -1,8 +0,0 @@
-
-class QueryFormulator:
-
-    def __init__(self):
-        pass
-
-    def get_keywords(self, query):
-        raise NotImplementedError
