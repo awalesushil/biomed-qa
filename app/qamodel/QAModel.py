92c84111a001e55085ca0c1e8d2d68741e1e480e
Sushil Awale <ssushil.awale@gmail.com>
2022-07-05 22:41:16 +0200

    Components stub files

---
+++ app/qamodel/QAModel.py
@@ -0,0 +1,7 @@
+class QAModel:
+
+    def __init__(self):
+        pass
+
+    def get_answers(self, passages):
+        raise NotImplementedError
---
+++ app/queryformulator/QueryFormulator.py
@@ -0,0 +1,8 @@
+
+class QueryFormulator:
+
+    def __init__(self):
+        pass
+
+    def get_keywords(self, query):
+        raise NotImplementedError
