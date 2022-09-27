5a289b64449b926fba27d09346a61bc989ec52ef
Sushil Awale <ssushil.awale@gmail.com>
2022-09-27 22:13:33 +0200

    Updated example questions list

--- app/resources.py
+++ app/resources.py
@@ -240,12 +240,12 @@ questions = [
     "Which syndromes are associated with mutations in the EZH2 gene?",
     "Which is the supplemental antioxidant in athletes?",
     "Which signaling pathways have been associated with medulloblastoma formation and growth?",
-    "List proteins of lipids droplets",
+    "What is the ideal vascular access for hemodialysis because it presents fewer complications? ",
     "What is the treatment of acute myocarditis?",
     "Do archaeal genomes contain one or multiple origins of replication?",
     "Which is the most known bacterium responsible for botulism",
-    "What is oprozomib?",
+    "What is paracetamol?",
     "Which is the most common cause of sudden cardiac death in young athletes?",
     "Which is the gene mutated in type 1 neurofibromatosis?",
-    "What is an endoscopic retrograde cholangiopancreatography?"
+    "What is the main cause of a stroke?"
 ]
